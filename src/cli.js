ae47afbbd667373f1b77178912dc1929972b2c70
Ilia Urvachev <rtm@ctrlz.ru>
2024-04-04 02:35:25 +0200

    fix(cli): refactor output, add support for multiple values and option `-s | --sort` for short output

--- src/cli.js
+++ src/cli.js
@@ -7,14 +7,34 @@ const chalk = require("chalk");
 
 const unitGolf = require("./index");
 
-const args = minimist(process.argv.slice(2));
+const args = minimist(
+  process.argv.slice(2),
+  {
+    alias: {
+      t: "tolerance",
+      w: "width",
+      h: "height",
+      s: "short"
+    },
+    default: {
+      tolerance: 0.2,
+      width: 400,
+      height: 300,
+      short: false
+    }
+  }
+);
 
 const {
-  _: [input],
+  _: input,
   tolerance,
   width,
-  height
+  height,
+  short
 } = args;
+
+const isMulti = input.length > 1;
+let longestInput = isMulti ? Math.max(...input.map(i => i.toString().length)) : 0;
 
 const spinner = ora();
 spinner.start();
@@ -34,14 +54,51 @@ const renderBest = option => {
 
 const renderRest = option => {
   const { string, pixelOffset } = option;
-  return [string, renderOffset(pixelOffset)].join(" ");
+  return [string, renderOffset(pixelOffset)].join(" ").trimEnd();
 };
 
-unitGolf({ input, tolerance, width, height }).then(([best, ...rest]) => {
-  spinner.stop();
-  console.log("");
-  console.log(renderBest(best));
-  console.log("");
-  console.log(rest.map(renderRest).join("\n"));
-  console.log("");
-});
+const renderRestColumns = values => {
+  const termWidth = process.stdout.columns || 80;
+  const maxLength = Math.max(...values.map(v => v.length)) + 1;
+  const columns = Math.max(1, Math.floor(termWidth / (maxLength)));
+  return values
+    .reduce(
+      (acc, v, i) => {
+        if (i % columns === 0) {
+          acc.push([]);
+        }
+        acc[acc.length - 1].push(v.trimEnd().padEnd(maxLength));
+        return acc;
+      },
+      []
+    )
+    .map(line => line.join(""))
+    .join("\n");
+};
+
+Promise
+  .all(
+    input
+      .map(item => item.toString())
+      .map(
+        input => unitGolf({ input, tolerance, width, height }).then(([best, ...rest]) => ({
+            input,
+            best: renderBest(best),
+            rest: short ? undefined : renderRestColumns(rest.map(renderRest))
+          })
+        )
+      )
+  )
+  .then(results => {
+    spinner.stop();
+    results.forEach(({ input, best, rest }) => {
+      console.log(
+        isMulti ? `${chalk.hex("#bfc34a").bold(input.padStart(longestInput))} =>` : "",
+        best
+      );
+
+      if (!short) {
+        console.log(`\n${rest}\n`);
+      }
+    });
+  });
